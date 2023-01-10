9d199d0a8915d503eee5009cf741d6109a82e0d8
Alejae1998 <alejagr1998@gmail.com>
2023-01-09 18:20:15 -0800

    rendering the zodiacs and the info on the page, missing css

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Main.css';
-// import { zodiac } from '../../data.js';
-// import ZodiacSign from '../ZodiacSign/ZodiacSign.js';
+import { zodiac } from '../../data.js';
+import ZodiacSign from '../ZodiacSign/ZodiacSign.js';
 import background from '../../background.jpeg';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {zodiac.map((sign) => (
+        <ZodiacSign key={sign.id} {...sign} />
+      ))}
+    </main>
+  );
 }
